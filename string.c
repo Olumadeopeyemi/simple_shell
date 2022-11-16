30e1217358b18b44d476c7425c8a10d40c3acd1e
Ukokpala <okpalaukamka480@gmail.com>
2022-11-16 10:47:51 -0800

    simple shell tasks

--- string.c
+++ string.c
@@ -1,75 +1,88 @@
 #include "shell.h"
-
 /**
- * _strlen - returns the length of a string
- * @s: the string whose length to check
+ * _strcmp - compares two strings
+ * @s1: char pointer
+ * @s2: char pointer
  *
- * Return: integer length of string
- */
-int _strlen(char *s)
-{
-	int i = 0;
-
-	if (!s)
-		return (0);
-
-	while (*s++)
-		i++;
-	return (i);
-}
-
-/**
- * _strcmp - performs lexicogarphic comparison of two strangs.
- * @s1: the first strang
- * @s2: the second strang
- *
- * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
+ * _strcmp: compares two strings at the same spot on each string
+ * Return: positive, negative, or 0 if theyre the same
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && *s2)
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (*s1 != *s2)
-			return (*s1 - *s2);
-		s1++;
-		s2++;
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
 	}
-	if (*s1 == *s2)
-		return (0);
-	else
-		return (*s1 < *s2 ? -1 : 1);
+	return (s1[i] - s2[i]);
 }
-
 /**
- * starts_with - checks if needle starts with haystack
- * @haystack: string to search
- * @needle: the substring to find
- *
- * Return: address of next char of haystack or NULL
+ * _strlen - counts the length of the string
+ * @str: string
+ * Return: length of the string
  */
-char *starts_with(const char *haystack, const char *needle)
+size_t _strlen(char *str)
 {
-	while (*needle)
-		if (*needle++ != *haystack++)
-			return (NULL);
-	return ((char *)haystack);
-}
+	size_t length = 0;
 
+	while (*str++)
+		length++;
+
+	return (length);
+}
 /**
- * _strcat - concatenates two strings
- * @dest: the destination buffer
- * @src: the source buffer
- *
- * Return: pointer to destination buffer
+ * _strcpy - string copy
+ * @dest: dest string
+ * @src: source string
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *ptr = dest;
+
+	while (*src)
+		*ptr++ = *src++;
+	*ptr = *src;
+
+	return (dest);
+}
+/**
+ * _strdup - string duplication
+ * @str: string
+ * Return: string
+ */
+char *_strdup(char *str)
+{
+	char *new;
+
+	if (str == NULL)
+		return (NULL);
+	new = malloc(sizeof(char) * (_strlen(str) + 1));
+	if (new == NULL)
+		return (NULL);
+	_strcpy(new, str);
+	return (new);
+}
+/**
+ * _strcat - string concnat
+ * @dest: destination
+ * @src: source
+ * Return: concated string
  */
 char *_strcat(char *dest, char *src)
 {
-	char *ret = dest;
+	char *ptrs = dest;
 
-	while (*dest)
-		dest++;
+	while (*ptrs)
+		ptrs++;
 	while (*src)
-		*dest++ = *src++;
-	*dest = *src;
-	return (ret);
+		*ptrs++ = *src++;
+	*ptrs = 0;
+	return (dest);
 }
